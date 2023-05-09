3988a73454db1f6443029efd85557f0afc3ba660
Manish <manishdandotiya61@gmail.com>
2023-05-09 08:46:27 +0530

    New code login

--- src/test/java/payload/ReUsable.java
+++ src/test/java/payload/ReUsable.java
@@ -1,5 +1,14 @@
 package payload;
 
+import io.restassured.path.json.JsonPath;
+
 public class ReUsable {
+	
+	public static JsonPath rawToJson(String response) {
+		
+		JsonPath js= new JsonPath(response);
+		return js;
+		//test
+	}
 
 }
